f633fccdabfcf5c4cc305e40f6969ede22cebf24
Moh Agus Setiawan <mohagussetiaone@gmail.com>
2024-02-29 22:04:34 +0700

    update layouts & path vercel.json

--- src/layouts/layouts.jsx
+++ src/layouts/layouts.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import { Outlet, useLoaderData, useLocation } from 'react-router-dom';
 import { routes } from '../routes';
-import Sidebar from '../components/sidebar/index';
+import Sidebar from 'components/sidebar';
 import Header from 'components/Header';
 import Footer from 'components/footer';
 
