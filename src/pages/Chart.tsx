4a66f9e70e452f910874635f86e3b2f35228f300
Moh Agus Setiawan <agus@saas-studios.com>
2024-09-20 08:20:25 +0700

    update: fixed

--- src/pages/Chart.tsx
+++ src/pages/Chart.tsx
@@ -9,7 +9,6 @@ const Chart: React.FC = () => {
   return (
     <DefaultLayout>
       <Breadcrumb pageName="Chart" />
-
       <div className="grid grid-cols-12 gap-4 md:gap-6 2xl:gap-7.5">
         <ChartOne />
         <ChartTwo />
